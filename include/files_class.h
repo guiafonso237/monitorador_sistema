e9c3341a506279a845e7cb62065ae5870dacffb7
guiafonso237 <guilhermeafonsodamasceno@gmail.com>
2026-04-17 14:21:45 -0300

    adicionado a classe file

---
+++ include/files_class.h
@@ -0,0 +1,28 @@
+#ifndef FILES_CLASS_H
+#define FILES_CLASS_H
+
+#include <string>
+#include <fstream>
+
+class File{
+    private:
+        std::fstream fin;
+        std::string caminho, chave;
+        int linha;
+    
+    public:
+        void setCaminho(std::string caminho);
+        void setLinha(int linha);
+        void setChave(std::string chave);
+
+        std::string getCaminho();
+        int getLinha();
+        std::string getChave();
+
+        std::string obterString();
+        //int obterLinha();
+        void abrirArquivo();
+        void const fecharArquivo();
+};
+
+#endif
---
+++ src/files_class.cpp
@@ -0,0 +1,55 @@
+#include "../include/files_class.h"
+
+
+    void File::setCaminho(std::string caminho){
+        this->caminho = caminho;
+    }
+
+    void File::setLinha(int linha){
+        this->linha = linha;
+    }
+
+    void File::setChave(std::string chave){
+        this->chave = chave;
+    }
+
+    std::string File::getCaminho(){
+        return this->caminho;
+    }
+
+    int File::getLinha(){
+        return this->linha;
+    }
+
+    std::string File::getChave(){
+        return this->chave;
+    }
+
+    void File::abrirArquivo(){
+        fin.open(this->caminho);
+    }
+
+    void const File::fecharArquivo(){
+        fin.close();
+    }
+
+    std::string File::obterString(){
+        std::string lixo, resultado;
+
+        try{
+            if(!fin.is_open()){
+                throw -1;
+            }
+
+            for(int i = 1; i < this->linha; i++){
+                std::getline(fin, lixo);
+            }
+
+            std::getline(fin, resultado);
+            return resultado;
+        }
+        catch(int erro){
+            return "";
+        }
+    }
+
